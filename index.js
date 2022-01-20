299481cbab7364a3c3e6ae3b87c2fce77ddd9a61
saurabhchirde <saurabh123.chirde@gmail.com>
2022-01-20 20:41:48 +0530

    fix: chips addition & deletion

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 const chipCloseBtn = document.querySelectorAll(".chip-close-btn");
 const textValue = document.querySelector("input");
+const chipEle = document.querySelector(".chips-element");
 
 const heartAll = document.querySelectorAll(".heart");
 const shareAll = document.querySelectorAll(".share");
 const optAll = document.querySelectorAll(".opt");
+
+// chips
+
+let chipsItems = ["Apple", "Banana", "Milkshake"];
+
+function remove(i) {
+  chipsItems = chipsItems.filter((item) => chipsItems.indexOf(item) != i);
+  render();
+}
+
+function render() {
+  chipEle.innerHTML = "";
+  chipsItems.map((item, index) => {
+    chipEle.innerHTML += `<div class="chips-text">${item}         
+<a href="javascript: remove(${index})"><button class="chip-close-btn"><i class="fas fa-times-circle"></i>
+    </button></a>
+  </div>`;
+  });
+}
 
 document.addEventListener("keydown", (e) => {
   if (e.keyCode === 13) {
     if (textValue.value === "") alert("Enter some input");
     else {
       const chipText = textValue.value;
-      document.querySelector(
-        ".chips-element"
-      ).innerHTML += `<div class="chips-text">${chipText}         
-          <button class="chip-close-btn">
-            <i class="fas fa-times-circle"></i>
-          </button>
-        </div>`;
+      chipsItems.push(chipText);
+      render();
+      textValue.value = "";
     }
   }
 });
+
+window.onload = function () {
+  render();
+  textValue.focus();
+};
+
+// card nav
 
 heartAll[0].addEventListener("click", () => {
   heartAll[0].classList.toggle("heart-after");
