6e47e841ef1ac90fb2937c440d4183e436e884a7
saurabhchirde <saurabh123.chirde@gmail.com>
2022-01-23 01:16:55 +0530

    fix: tabs flights status

--- js/tabs.js
+++ js/tabs.js
@@ -3,19 +3,33 @@ const tripsBtn= document.querySelector(".trips-btn");
 const flightsBtn= document.querySelector(".flight-btn")
 
 exploreBtn.addEventListener("click",()=>{
-    console.log("clicked explore");
     document.querySelector(".tab-explore").classList.remove("hide");
     document.querySelector(".tab-trips").classList.add("hide");
+
+    exploreBtn.classList.add("tab-active");
+    tripsBtn.classList.remove("tab-active");
+    flightsBtn.classList.remove("tab-active");
 });
 
 tripsBtn.addEventListener("click",()=>{
-    console.log("clicked trips");
     document.querySelector(".tab-explore").classList.add("hide");
     document.querySelector(".tab-trips").classList.remove("hide");
+    document.querySelector("tab-active-explore").classList.add("tab-active-trips");
+    document.querySelector("tab-active-explore").classList.remove("tab-active-flights");
+
+    exploreBtn.classList.remove("tab-active");
+    tripsBtn.classList.add("tab-active");
+    flightsBtn.classList.remove("tab-active");
 });
 
 flightsBtn.addEventListener("click",()=>{
-    console.log("clicked flight");
     document.querySelector(".tab-explore").classList.add("hide");
     document.querySelector(".tab-trips").classList.add("hide");
+    document.querySelector(".tab-flight").classList.remove("hide");
+    document.querySelector("tab-active-explore").classList.add("tab-active-flights");
+    document.querySelector("tab-active-explore").classList.remove("tab-active-trips");
+
+    exploreBtn.classList.remove("tab-active");
+    tripsBtn.classList.remove("tab-active");
+    flightsBtn.classList.add("tab-active");
 });
