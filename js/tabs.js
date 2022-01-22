a77be0bc09c6f11033ba1f7d739d9527b94bed7c
saurabhchirde <saurabh123.chirde@gmail.com>
2022-01-22 23:19:15 +0530

    fix: tab hide

--- js/tabs.js
+++ js/tabs.js
@@ -1,12 +1,21 @@
-const exploreBtn= document.querySelector(".explore-btn")
-const tripsBtn= document.querySelector(".trips-btn")
+const exploreBtn= document.querySelector(".explore-btn");
+const tripsBtn= document.querySelector(".trips-btn");
+const flightsBtn= document.querySelector(".flight-btn")
 
 exploreBtn.addEventListener("click",()=>{
-    document.querySelector(".explore").classList.remove("hide");
+    console.log("clicked explore");
+    document.querySelector(".tab-explore").classList.remove("hide");
     document.querySelector(".tab-trips").classList.add("hide");
-})
+});
 
 tripsBtn.addEventListener("click",()=>{
-    document.querySelector(".explore").classList.add("hide");
+    console.log("clicked trips");
+    document.querySelector(".tab-explore").classList.add("hide");
     document.querySelector(".tab-trips").classList.remove("hide");
-})
+});
+
+flightsBtn.addEventListener("click",()=>{
+    console.log("clicked flight");
+    document.querySelector(".tab-explore").classList.add("hide");
+    document.querySelector(".tab-trips").classList.add("hide");
+});
